5c11ab5526fb829a928a1c6b4b4a4b0824959131
Bvtttks <102364966+Bvtttks@users.noreply.github.com>
2023-01-26 23:59:02 +0300

    main.cpp
    task complete!

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,22 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    string line;
+    for(int i = 2; i < 994; ++i){
+        line = to_string(i);
+        for(char c : line) {
+            if (c == '3')
+                ++count;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
